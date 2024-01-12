dd5a0ed24a06507a8e141977c188d06387b047ea
RyanDC1 <ryan.l.dacosta@gmail.com>
2024-01-12 21:34:35 +0530

    Fixed build issue

--- src/components/chatInterface/PromptEditor.tsx
+++ src/components/chatInterface/PromptEditor.tsx
@@ -50,7 +50,6 @@ const PromptEditor = forwardRef<PromptEditorRef, Props>((props, ref) => {
     ), [inputMode])
 
     useEffect(() => {
-        const typewriter = generateDyanmicPlaceholder({ id: 'chat-prompt', options: PromptHelpers })
         const typewriter = generateDynamicPlaceholder({ id: 'chat-prompt', options: PromptHelpers })
         return () => {
             typewriter.stop()
